0997a121a90020d2a4a535949717421a0cb18c50
tgdds <tg.dds.marko@gmail.com>
2026-02-22 13:58:41 +0900

    wip privates

--- lib/engine/game/g_1890/entities.rb
+++ lib/engine/game/g_1890/entities.rb
@@ -9,8 +9,8 @@ module Engine
             name: '有馬鉄道',
             value: 20,
             revenue: 5,
-            desc: '配置制限:有馬(D7) \n 企業に売られたら有馬にタイル配置',
-            sym: 'AR',
+            desc: "配置制限:有馬(D7) \n 企業に売られたら有馬にタイル配置",
+            sym: '有電',
             abilities: [{ type: 'blocks_hexes', owner_type: 'player', hexes: ['D7'] },
                         {
                           type: 'tile_lay',
@@ -23,91 +23,67 @@ module Engine
             color: nil,
           },
           {
-            name: 'Mitsubishi Ferry',
-            value: 30,
-            revenue: 5,
-            desc: 'Player owner may place the port tile on a coastal town (B11,'\
-                  ' G10, I12, or J9) without a tile on it already, outside of '\
-                  'the operating rounds of a corporation controlled by another '\
-                  'player. The player need not control a corporation or have '\
-                  'connectivity to the placed tile from one of their '\
-                  'corporations. This does not close the company.',
-            sym: 'MF',
-            abilities: [
-              {
-                type: 'tile_lay',
-                when: %w[stock_round owning_player_track or_between_turns],
-                hexes: %w[B11 G10 I12 J9],
-                tiles: ['437'],
-                owner_type: 'player',
-                count: 1,
-              },
-            ],
+            name: '神戸市電',
+            value: 40,
+            revenue: 10,
+            desc: '配置制限:神戸(F5)',
+            sym: '神電',
+            abilities: [{ type: 'blocks_hexes', owner_type: 'player', hexes: ['F5'] }],
             color: nil,
           },
           {
-            name: 'Ehime Railway',
-            value: 40,
-            revenue: 10,
-            desc: 'When this company is sold to a corporation, the selling '\
-                  'player may immediately place a green tile on Ohzu (C4), '\
-                  'in addition to any tile which it may lay during the same '\
-                  'operating round. This does not close the company. Blocks '\
-                  'C4 while owned by a player.',
-            sym: 'ER',
-            abilities: [{ type: 'blocks_hexes', owner_type: 'player', hexes: ['C4'] },
+            name: '阪堺電鉄',#TODO 4フェイズにプレイヤー持ちでも封鎖が消えるかどうか
+            value: 70,
+            revenue: 15,
+            desc: '配置制限:堺(J11)大阪南(I10) \n 第4フェイズ(5列車購入時)に閉鎖しない　ただし収入は5になり、売却できず、株券枚数制限に含み続ける',
+            sym: '堺電',
+            abilities: [{ type: 'blocks_hexes', owner_type: 'player', remove: '5',hexes: %w[J11 I10] },
+                        { type: 'close', on_phase: 'never'},
                         {
-                          type: 'tile_lay',
-                          hexes: ['C4'],
-                          tiles: %w[12 13 14 15 205 206],
-                          when: 'sold',
-                          owner_type: 'corporation',
-                          count: 1,
+                          type: 'revenue_change',
+                          revenue: 5,
+                          on_phase: '5',
                         }],
             color: nil,
           },
           {
-            name: 'Sumitomo Mines Railway',
-            value: 50,
-            revenue: 15,
-            desc: 'Owning corporation may ignore building cost for mountain '\
-                  'hexes which do not also contain rivers. This does not close '\
-                  'the company.',
-            sym: 'SMR',
-            abilities: [
-              {
-                type: 'tile_discount',
-                discount: 80,
-                terrain: 'mountain',
-                owner_type: 'corporation',
-              },
-            ],
-            color: nil,
-          },
-          {
-            name: 'Dougo Railway',
-            value: 60,
-            revenue: 15,
-            desc: 'Owning player may exchange this private company for a 10% '\
-                  'share of Iyo Railway from the initial offering.',
-            sym: 'DR',
-            abilities: [
-              {
-                type: 'exchange',
-                corporations: ['IR'],
-                owner_type: 'player',
-                when: 'any',
-                from: 'ipo',
-              },
-            ],
-            color: nil,
-          },
-          {
-            name: 'South Iyo Railway',
-            value: 80,
+            name: '阪神国道軌道',
+            value: 110,
             revenue: 20,
-            desc: 'No special abilities.',
-            sym: 'SIR',
+            desc: '配置制限なし　阪神電鉄の株券1株が付属',
+            sym: '阪国',
+            abilities: [
+              { type: 'shares', shares: 'HS_1' }
+            ],
+            color: nil,
+          },
+          {
+            name: '京津鉄道',
+            value: 160,
+            revenue: 25,
+            desc: '配置制限:京都(B17)京都の東(B19) \n 京阪電鉄の株券1株が付属',
+            sym: '京津',
+            abilities: [
+              { type: 'blocks_hexes', owner_type: 'player', hexes: %w[J11 I10] },          
+              { type: 'shares', shares: 'KH_1' }
+            ],
+            color: nil,
+          },
+          {
+            name: '大阪市電',#TODO 大阪地下鉄の最初の手番がくれば、大阪市内の3ヘックスへのタイルの置き換えは、自由になるものとします。と、大阪市電は、プレーヤーに購入された時点で、額面価格が¥0 に変更されます。
+            value: 220,
+            revenue: 40,
+            desc: '配置制限:大阪北(G12)大阪西(H11)大阪東(H13)  '\
+                  '購入プレーヤーは大阪地下鉄の株価を設定し、社長株を受け取ります。'\
+                  '大阪地下鉄が設立されても、列車を購入しなければ、大阪市電は閉鎖されず、大阪地下鉄は、配当を受け続けることができます。'\
+                  'しかし、大阪地下鉄は、運営ラウンドの自社の手番で無配を繰り返すため、株価は下がり続けます。'\
+                  '大阪地下鉄の最初の手番がくれば、大阪市内の3ヘックスへのタイルの置き換えは、自由になるものとします。'\
+                  '大阪地下鉄が列車を購入すると大阪市電は閉鎖されます。この個人会社は、公共会社が購入することが出来ません。',
+            abilities: [{ type: 'blocks_hexes', owner_type: 'player', hexes: %w[G12 H11 H13] },
+                        { type: 'close', when: 'bought_train', corporation: 'HT' },
+                        { type: 'no_buy' },
+                        { type: 'shares', shares: 'HT_0' }],
+            sym: '市電',
             color: nil,
           },
           {
