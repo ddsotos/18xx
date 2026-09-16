3501450b8aa1105e841b336ded0d1f8c453be26d
ddsotos <63486885+ddsotos@users.noreply.github.com>
2026-09-16 17:36:05 +0900

    Use unambiguous matcher block syntax

--- spec/lib/engine/game/g_rotla/track_tile_manifest_spec.rb
+++ spec/lib/engine/game/g_rotla/track_tile_manifest_spec.rb
@@ -22,11 +22,11 @@ describe Engine::Game::GRotLA::TrackTileManifest do
     expect(tiles.map { |tile| tile[:id] }.uniq.size).to eq(tiles.size)
     expect(tiles.map { |tile| tile[:source_group] }.sort).to eq((1..54).to_a)
     expect(tiles).to all(
-      satisfy do |tile|
-        tile[:count].positive? &&
+      satisfy {
+        |tile| tile[:count].positive? &&
           tile[:source_image].is_a?(Integer) &&
           tile[:source_top].size == 2
-      end
+      },
     )
   end
 
